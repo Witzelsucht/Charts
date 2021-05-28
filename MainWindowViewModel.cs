72e7bb3e372745d7cba3c20657c9374c929f5f98
Konrad <Witzelsucht@users.noreply.github.com>
2021-05-28 22:17:57 +0200

    initial commit

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using LiveCharts;
+using LiveCharts.Wpf;
+using System.Linq;
+using System.Windows.Media;
+
+namespace Charts
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public List<Measurement> Measurements { get; set; }
+
+        public RelayCommand Load { get; set; }
+
+        private SeriesCollection _seriesViews;
+
+        public SeriesCollection SeriesViews
+        {
+            get { return _seriesViews; }
+            set { Set(ref _seriesViews, value); }
+        }
+
+
+        private bool _isLoaded;
+        public bool IsLoaded
+        {
+            get { return _isLoaded; }
+            set { Set(ref _isLoaded, value); }
+        }
+
+        private bool _isError;
+        public bool IsError
+        {
+            get { return _isError; }
+            set { Set(ref _isError, value); }
+        }
+
+        private string _errorMessage;
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set { Set(ref _errorMessage, value); }
+        }
+
+        public List<string> Labels { get; set; } = new List<string>() { "Voltage", "Current", "Temperature" };
+
+        public MainWindowViewModel()
+        {
+            Measurements = new List<Measurement>();
+            Load = new RelayCommand(_Load);
+        }
+
+        private void _Load()
+        {
+            var dialog = new OpenFileDialog()
+            {
+                Title = "Select a file",
+                Filter = "Text Filest (*.txt)|*.txt"
+            };
+            if (dialog.ShowDialog() ?? false)
+            {
+                var path = dialog.FileName;
+                var line = "";
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while ((line = sr.ReadLine()) != null )
+                    {
+                        try
+                        {
+                            var arr = line.Replace(" ", string.Empty).Replace(".",",").Split(';');
+                            var measurement = new Measurement()
+                            {
+                                Voltage = float.Parse(arr[0]),
+                                Current = float.Parse(arr[1]),
+                                Temperature = float.Parse(arr[2]),
+                                Time = int.Parse(arr[3])
+                            };
+                            Measurements.Add(measurement);
+                        }
+                        catch(Exception e)
+                        {
+                            IsError = true;
+                            ErrorMessage = e.Message;
+                        }
+                    }
+                    SeriesViews = new SeriesCollection
+                    {
+                       new LineSeries
+                       {
+                           Title = "Voltage",
+                           Values = new ChartValues<float>(Measurements.Select(m => m.Voltage).ToArray()),
+                           Stroke = new SolidColorBrush(Color.FromRgb(0,0,255)),
+                           Fill = new SolidColorBrush(Color.FromArgb(0,0,0,0))
+
+                       },
+                       new LineSeries
+                       {
+                           Title = "Current",
+                           Values = new ChartValues<float>(Measurements.Select(m => m.Current).ToArray()),
+                           Stroke = new SolidColorBrush(Color.FromRgb(0,255,0)),
+                           Fill = new SolidColorBrush(Color.FromArgb(0,0,0,0))
+
+                       },
+                       new LineSeries
+                       {
+                           Title = "Temperature",
+                           Values = new ChartValues<float>(Measurements.Select(m => m.Temperature).ToArray()),
+                           Stroke = new SolidColorBrush(Color.FromRgb(255,0,0)),
+                           Fill = new SolidColorBrush(Color.FromArgb(0,0,0,0))
+                       }
+                    };
+                    IsLoaded = true;
+                }
+            }
+        }
+    }
+
+    public class Measurement
+    {
+        public float Voltage { get; set; }
+        public float Current { get; set; }
+        public float Temperature { get; set; }
+        public int Time { get; set; }
+    }
+}
---
+++ ViewModelBase.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Charts
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string? propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        protected bool Set<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        private string name;
+        public string Name
+        {
+            get => name;
+            set => Set(ref name, value);
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        private Action methodToExecute;
+        private Func<bool> canExecuteEvaluator;
+        public RelayCommand(Action methodToExecute, Func<bool> canExecuteEvaluator)
+        {
+            this.methodToExecute = methodToExecute;
+            this.canExecuteEvaluator = canExecuteEvaluator;
+        }
+        public RelayCommand(Action methodToExecute)
+            : this(methodToExecute, null)
+        {
+        }
+        public bool CanExecute(object parameter)
+        {
+            if (canExecuteEvaluator == null)
+            {
+                return true;
+            }
+            else
+            {
+                bool result = this.canExecuteEvaluator.Invoke();
+                return result;
+            }
+        }
+        public void Execute(object parameter)
+        {
+            methodToExecute.Invoke();
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        readonly Action<T> _execute = null;
+        readonly Predicate<T> _canExecute = null;
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute((T)parameter);
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+    }
+}
